70fb9f21bfef5d99d771244155f61b3330d3e3cd
Farewell-CK <3345710651@qq.com>
2025-06-25 22:10:43 +0800

    修改启动方式

--- run_opening_service.py
+++ run_opening_service.py
@@ -1,9 +1,9 @@
 import uvicorn
-from api.opening_service import app
+# from api.opening_service import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "api.opening_service:app",
         host="0.0.0.0",
         port=11434,
         reload=True,
